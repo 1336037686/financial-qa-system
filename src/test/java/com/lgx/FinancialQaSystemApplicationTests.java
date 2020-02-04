6c439afed1a203c4c018dcfe0a5c75f9690a236e
LGX <1336037686@qq.com>
2020-02-04 22:01:56 +0800

    初始化工程

---
+++ src/test/java/com/lgx/FinancialQaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialQaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
